af0cc51a91e2826425c21da34dbb3196b25da96a
Jose-Rodriguez-He <jose.rodriguez1901@alumnos.ubiobio.cl>
2022-12-11 23:35:32 -0300

    Envio de correos desde base de datos

--- controllers/mailerController.js
+++ controllers/mailerController.js
@@ -1,12 +1,11 @@
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
-const User = require("../controllers/userController")
+const User = require('../models/user');
 dotenv.config();
 
 const sendmail = (req, res) => {
-    const { message } = req.body
+    const { from, asunto, texto} = req.body
     const token = process.env.PW
-    //const mail = 'felipe.vasquez1902@alumnos.ubiobio.cl'
     if (!token) {
         return res.status(400).send({ message: "No se ha entregado la contraseña de aplicación para el correo" })
     }
@@ -19,28 +18,40 @@ const sendmail = (req, res) => {
             pass: token
         }
     })
-    let directory =[
-        'alejandro.torres1901@alumnos.ubiobio.cl',
-        'jose.rodriguez1901@alumnos.ubiobio.cl',
-        'moises.salgado1901@alumnos.ubiobio.cl',
-        'felipe.vasquez1902@alumnos.ubiobio.cl'
-    ]
-    const mailOptions = {
-        from: `Grupo 12`,
-        to: directory,
-        subject: 'Prueba de correos',
-        text: `La junta de vecinos tiene el siguiente mensaje: ${message}`,
-        //html: `
-        //    <h1>Felicitaciones, has enviado un correo</h1>
-        //    <p>${message}</p>
-        //    `
-    }
-    transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-            return res.status(400).send({ message: 'Error al enviar el correo' })
+
+    User.find({}, (error, user) => {
+        if (error) {
+            return res.status(400).send({ message: 'Error al obtener los emails de los usuarios' });
         }
-        return res.status(200).send({ message: 'Mensaje enviado' })
+        user.forEach(user => {
+            console.log(user.email);
+            const mailOptions = {
+                from: `Enviado por ${from}`,
+                to: user.email,
+                subject: `${asunto}`,
+                text: 'La junta de vecinos tiene un aviso:',
+                html: `
+                    <h1>Tu mensaje es: </h1>
+                    <p>${texto}</p>
+                    `
+            }
+            transporter.sendMail(mailOptions, (err, info) => {
+                if (err) {
+                    return res.status(400).send({ message: 'Error al enviar el correo' })
+                }
+                return res.status(200).send({ message: 'Mensaje enviado' })
+            })
+        });
+        return res.status(200).send('');
     })
+    //const correos = emails.map(email => emails.email);
+
+    // let directory =[
+    //     'alejandro.torres1901@alumnos.ubiobio.cl',
+    //     'jose.rodriguez1901@alumnos.ubiobio.cl',
+    //     'moises.salgado1901@alumnos.ubiobio.cl',
+    //     'felipe.vasquez1902@alumnos.ubiobio.cl'
+    // ]
 
     transporter.verify().then(() => {
         console.log('Servidor de correos habilitado')
