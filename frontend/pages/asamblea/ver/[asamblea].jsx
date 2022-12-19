61446df8028feb65ecb8765a46e58e71ba3c5241
Felipe Vasquez <minelyne@gmail.com>
2022-12-19 16:52:27 -0300

    crud asamblea, revisar eliminar

--- frontend/pages/asamblea/ver/[asamblea].jsx
+++ frontend/pages/asamblea/ver/[asamblea].jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import axios from 'axios'
-import { Container, Heading, Tbody,Stack,HStack,Button } from '@chakra-ui/react'
+import { Container, Heading, Tbody,Stack,HStack,Button,RadioGroup,Radio } from '@chakra-ui/react'
 import ShowInfo from '../../../components/ShowInfo'
+import Swal from 'sweetalert2'
 
 export async function getServerSideProps(context){
     console.log(context.params.asamblea)
@@ -16,8 +17,9 @@ export async function getServerSideProps(context){
     } catch (error) {
         console.log("ERROR",error)
         return{
-            props:{
-                data:null
+            redirect:{
+                destination: '/asamblea/ver',
+                permanent: true
             }
         }
     }
@@ -26,21 +28,57 @@ export async function getServerSideProps(context){
 const asamblea = (data) => {
     const router = useRouter()
     const [asambleas] = useState(data)
+    const[values, setValues] = useState({
+    })
 
+    const onChange = async (e) =>{
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
+    }
+
+
+    const eliminarAsamblea = async () =>{
+        try {
+            const response = await axios.delete(`${process.env.API_URL}/asamblea/delete/${asambleas.asambleaId._id}`,values)//revisar
+            if (response.status === 200){
+                Swal.fire({
+                    title: 'Asamblea eliminada',
+                    text: 'La asamblea se ha eliminado con exito',
+                    icon: 'success',
+                    confirmButtonText: 'Ok'
+                })
+            }
+            }catch (error) {
+            Swal.fire({
+                title: 'Error',
+                text: `La asamblea no se ha podido eliminar ${error}`,
+                icon: 'error',
+                confirmButtonText: 'Ok'
+            })
+        }
+    }
 
     return (
         <Container maxW="container.xl" centerContent>
             <Heading my={10}> {asambleas.asambleaId.name}</Heading>
-            <Stack w={"full"}>
-                <ShowInfo tag="Nombre" data={asambleas.asambleaId.name} color="blue.500" />
-                <ShowInfo tag="Tipo" data={asambleas.asambleaId.tipo} color="yellow.500" />
-                <ShowInfo tag="Archivos" data={asambleas.asambleaId.archivos} color="green.500" />
-            </Stack>
             <HStack w={"full"} py={10}>
-                <Button w={"full"} colorScheme={"teal"} onClick={() => router.push(`/producto/editar/${product._id}`)}>Editar</Button>
-                <Button w={"full"} colorScheme={"teal"} onClick={() => handleDelete()}>Eliminar</Button>
+                <Button w={"full"} colorScheme={"teal"} onClick={() => router.push(`/asamblea/editar/${asambleas.asambleaId._id}`)}>Editar</Button>
+                <Button w={"full"} colorScheme={"teal"} onClick={() => eliminarAsamblea()}>Eliminar</Button>
                 <Button w={"full"} colorScheme={"teal"} onClick={() => router.push("/asamblea/ver")}>Volver</Button>
             </HStack>
+            <Stack w={"full"}>
+                <ShowInfo tag="Nombre" data={asambleas.asambleaId.name} />
+                <ShowInfo tag="Tipo" data={asambleas.asambleaId.tipo} />
+                <ShowInfo tag="Archivos" data={asambleas.asambleaId.archivos}/>
+            </Stack>
+            <RadioGroup >
+                <HStack spacing='24px'>
+                <Radio value='user' onChange={onChange} name={"rolUsuario"}>user</Radio>
+                <Radio value='admin' onChange={onChange} name={"rolUsuario"}>admin</Radio>
+                </HStack>
+            </RadioGroup>
         </Container>
     )
 }
