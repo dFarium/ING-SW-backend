a5bed3d074b9aff826d0676173791872aa814e9c
Alejandro Torres <ale.torres.jofre@gmail.com>
2022-12-29 15:46:24 -0300

    Añade Links en Texto y modificaciones de diseño

--- frontend/pages/archivos/verArchivos.jsx
+++ frontend/pages/archivos/verArchivos.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react'
-import { Button, Container, HStack, Table, Thead, Tbody, Tfoot, Tr, Th, Td, Heading,Box, AccordionIcon } from '@chakra-ui/react'
+import { Button, Container, HStack, Table, Thead, Tbody, Tfoot, Tr, Th, Td, Heading,Box, AccordionIcon, Link, chakra } from '@chakra-ui/react'
 import axios from 'axios'
 import { useRouter } from 'next/router'
 import Arriba from '../../components/Arriba'
+//import { DownloadIcon } from '@chakra-ui/icons'
 
 const archivos= () => {
 
@@ -23,10 +24,13 @@ const archivos= () => {
             return(
                 <Tr key={archivos._id}>
                     <Td>{archivos.name}</Td>
-                    <Td>{archivos.asamblea.name}</Td>
+                    {/* <Td><Link color='blue.400' href={`${process.env.API_URL}/file/download/${archivos._id}`}>{archivos.name}</Link></Td> */}
+                    {/* <Td>{archivos.asamblea.name}</Td> */}
+                    <Td><Link color='blue.400' href={`/asamblea/ver/${archivos.asamblea._id}`}>{archivos.asamblea.name}</Link></Td>
                     <Td>{archivos.fecha}</Td>
                     <Td><Button onClick={()=>router.push(`${process.env.API_URL}/file/download/${archivos._id}`)}>Download</Button></Td>
-                    <Td><Button onClick={()=>router.push(`/asamblea/ver/${archivos.asamblea._id}`)}>Detalles</Button></Td>
+                    {/* <Td><DownloadIcon /></Td> */}
+                    <Td><Button onClick={()=>router.push(`${process.env.API_URL}/file/download/${archivos._id}`)}>Download</Button></Td>
                 </Tr>
             )
         })
@@ -36,15 +40,16 @@ const archivos= () => {
         <Box>
             <Arriba/>
             <Container maxW="container.xl">
-            <Heading textAlign={"center"} my={15}>Archivos</Heading>
-            <Button colorScheme={"teal"} float={'right'} onClick={()=>router.push('/')} >Volver</Button>
-            <Table variant="simple">
+            <Heading textAlign={"center"} my={16}>Historial de Actas</Heading>
+            <Button colorScheme={"teal"} my={15} mx={15} float={'left'} onClick={()=>router.push('/')} >Volver</Button>
+            <Table my={15} variant="simple">
                 <Thead>
                     <Tr>
-                        <Td>Nombre</Td>
+                        <Td>Archivo</Td>
                         <Td>Asamblea</Td>
                         <Td>Fecha</Td>
                         <Td>Descarga</Td>
+                        <Td>Eliminar</Td>
                     </Tr>
                 </Thead>
                 <Tbody>
