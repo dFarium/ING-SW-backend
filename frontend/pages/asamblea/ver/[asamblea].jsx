63f430243c8f6c1428d2d2d6c8ef8d87c38579df
Alejandro Torres <ale.torres.jofre@gmail.com>
2022-12-30 01:42:06 -0300

    añade menu desplegable de archivos existentes

--- frontend/pages/asamblea/ver/[asamblea].jsx
+++ frontend/pages/asamblea/ver/[asamblea].jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import axios from 'axios'
-import { Container, Heading, Tbody,Stack,HStack,Button,RadioGroup,Radio, Box } from '@chakra-ui/react'
+import { Container, Heading, Tbody,Stack,HStack,Button,RadioGroup,Radio, Box, Accordion, AccordionItem, AccordionButton, AccordionPanel,AccordionIcon, Table, Thead, Tr, Th, Td, Link} from '@chakra-ui/react'
 import ShowInfo from '../../../components/ShowInfo'
 import Swal from 'sweetalert2'
 import Arriba from '../../../components/Arriba'
@@ -28,8 +28,7 @@ export async function getServerSideProps(context){
 const asamblea = (data) => {
     const router = useRouter()
     const [asambleas] = useState(data)
-    const[values, setValues] = useState({
-    })
+    const[values, setValues] = useState({})
 
     const onChange = async (e) =>{
         setValues({
@@ -64,6 +63,68 @@ const asamblea = (data) => {
         }
     }
 
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    const [archivos, setArchivos] = useState([])
+
+    const getAsambleaArchivos = async (id_asamblea) => {
+        try {
+            const response = await axios.get(`${process.env.API_URL}/file/${id_asamblea}`)
+            setArchivos(response.data)
+        } catch (error) {
+            Swal.fire({
+                title: 'Sin Archivos',
+                text: `No existe ningun archivo asociado por el momento`,
+                icon: 'warning',
+                confirmButtonText: 'Ok'
+            })
+        }
+    }
+
+    const eliminarArchivos = async (id_archivo, id_asamblea) =>{
+        try {
+            //AGREGAR MODALs
+            const response = await axios.delete(`${process.env.API_URL}/file/delete/${id_archivo}/${id_asamblea}`)
+            if (response.status === 200){
+                Swal.fire({
+                    title: 'Archivo eliminado',
+                    text: 'El archivo se ha eliminado con exito',
+                    icon: 'success',
+                    confirmButtonText: 'Ok'
+                }).then((result)=>{
+                    if (result.isConfirmed){
+                        router.reload()
+                    }
+                })
+            }
+            }catch (error) {
+            Swal.fire({
+                title: 'Error',
+                text: `El archivo no se ha podido eliminar`,
+                icon: 'error',
+                confirmButtonText: 'Ok'
+            })
+        }
+    }
+
+    useEffect(()=>{
+        getAsambleaArchivos(asambleas.asambleaId._id)
+    }, [])
+
+    const showAsambleaArchivos = () =>{
+        return archivos.map(archivos =>{
+            return(
+                <Tr key={archivos._id}>
+                    <Td><Link color='blue.500' href={`${process.env.API_URL}/file/download/${archivos._id}`}>{archivos.name}</Link></Td>
+                    <Td>{archivos.fecha}</Td>
+                    <Td><Button onClick={()=>eliminarArchivos(archivos._id, asambleas.asambleaId._id)}>Eliminar</Button></Td>
+                </Tr>
+            )
+        })
+    }
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
     return (
         <Box>
             <Arriba/>
@@ -74,17 +135,46 @@ const asamblea = (data) => {
                         <Button w={"full"} colorScheme={"teal"} onClick={() => eliminarAsamblea()}>Eliminar</Button>
                         <Button w={"full"} colorScheme={"teal"} onClick={() => router.push("/asamblea/ver")}>Volver</Button>
                     </HStack>
-                <Stack w={"full"}>
-                    <ShowInfo tag="Nombre" data={asambleas.asambleaId.name} />
-                    <ShowInfo tag="Tipo" data={asambleas.asambleaId.tipo} />
-                    <ShowInfo tag="Archivos" data={asambleas.asambleaId.archivos}/>
-                </Stack>
                 <RadioGroup>
                     <HStack spacing='24px'>
                     <Radio value='user' onChange={onChange} name={"rolUsuario"}>user</Radio>
                     <Radio value='admin' onChange={onChange} name={"rolUsuario"}>admin</Radio>
                     </HStack>
                 </RadioGroup>
+
+                <Stack w={"full"}>
+                    <ShowInfo tag="Nombre" data={asambleas.asambleaId.name} />
+                    <ShowInfo tag="Tipo" data={asambleas.asambleaId.tipo} />
+                    <Accordion allowMultiple>
+                        <AccordionItem my={'15'}>
+                            <h2>
+                                <AccordionButton>
+                                    <Box as="span" flex='1' textAlign='left'>
+                                    Archivos Adjuntos
+                                    </Box>
+                                    <AccordionIcon />
+                                </AccordionButton>
+                            </h2>
+                            <AccordionPanel pb={'4'}>
+                                <Table my={15} variant="simple">
+                                    <Thead>
+                                        <Tr>
+                                            <Td>Archivo</Td>
+                                            <Td>Fecha</Td>
+                                            <Td>Eliminar</Td>
+                                        </Tr>
+                                    </Thead>
+                                    <Tbody>
+                                        {showAsambleaArchivos()}
+                                    </Tbody>
+                                </Table>
+                            </AccordionPanel>
+                        </AccordionItem>
+                    </Accordion>
+
+
+                </Stack>
+                
             </Container>
         </Box>
     )
