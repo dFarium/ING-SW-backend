5fc3e03e7c37ee2c1f19c0cfc6b2dc95ba513ca2
Alejandro Torres <ale.torres.jofre@gmail.com>
2023-01-01 17:32:46 -0300

    correccion de conflictos

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-import { Container,Heading,  Button, HStack,Link,Box, Icon} from '@chakra-ui/react'
-import React from 'react'
-import { useRouter } from 'next/router'
-import Arriba from '../components/Arriba'
-import { PhoneIcon, AddIcon, WarningIcon } from '../node_modules/@chakra-ui/icons'
-=======
 import { Container,Heading,  Button, HStack,Link, Box} from '@chakra-ui/react'
 import React from 'react'
 import { useRouter } from 'next/router'
 import Arriba from '../components/Arriba'
 import {checkToken} from '../data/usuario'
 // let token
->>>>>>> d5819a4de1fd6dc937e51cba0ff3bce192aa8a10
 
 export const getServerSideProps = async (context) => {
     try {
@@ -40,10 +32,6 @@ export const index = (data) => {
         <Box>
             <Container maxW="container.xl">
                 <Heading textAlign={"center"} my={15}>Bienvenido</Heading>
-                
-                <PhoneIcon />
-
-                
                 <HStack w={"full"} py={'28'}>
                     <Button w={"full"} colorScheme={"teal"} float={"right"} onClick={()=>router.push('/asamblea/ver')}>Asambleas</Button>
                     <Button w={"full"} colorScheme={"teal"} float={"right"} onClick={()=>router.push('/archivos/verArchivos')}>Historial de Actas</Button>
