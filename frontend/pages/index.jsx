e8afdf4196ec99d3bcf3e3be8cfb007e32231b38
Alejandro Torres <ale.torres.jofre@gmail.com>
2023-01-02 04:01:54 -0300

    Bienvenida de inicio

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,8 +1,12 @@
-import { Container,Heading,  Button, HStack,Link, Box} from '@chakra-ui/react'
+import { Container,Heading,  Button, HStack,Link, Box, Icon} from '@chakra-ui/react'
 import React from 'react'
 import { useRouter } from 'next/router'
 import Arriba from '../components/Arriba'
 import {checkToken} from '../data/usuario'
+import { GrMail } from 'react-icons/gr'
+import { HiUserGroup } from 'react-icons/hi'
+import { FaUserAlt } from 'react-icons/fa'
+import { AiFillFile } from 'react-icons/ai'
 // let token
 
 export const getServerSideProps = async (context) => {
@@ -25,21 +29,28 @@ export const getServerSideProps = async (context) => {
 export const index = (data) => {
     // console.log(token)
     const router = useRouter()
+
+    const indexButton = (nombre, ruta, nombreIcon) => {
+        return(
+            <Button mx='10' fontSize='30' size='lg' height='24' width='container.lg' colorScheme={'teal'} onClick={()=>router.push(ruta)}>
+                <Icon mx={'1.5'} w={'10'} h={'10'} as={nombreIcon}/>
+                    {nombre}
+            </Button>
+        )
+    }
     return (
         <Box >
             <Arriba token={data.existe}/>
         <Box >
             <Container maxW="container.xl">
-                <Heading textAlign={"center"} my={15}>Bienvenido</Heading>
-                <HStack w={"full"} py={'28'}>
-                    <Button w={"full"} colorScheme={"teal"} float={"right"} onClick={()=>router.push('/asamblea/ver')}>Asambleas</Button>
-                    <Button w={"full"} colorScheme={"teal"} float={"right"} onClick={()=>router.push('/archivos/verArchivos')}>Historial de Actas</Button>
-                    <Button w={"full"} colorScheme={"teal"} float={"right"} onClick={()=>router.push('/enviar_email/email')} >Mandar Avisos</Button>
+                <Heading  textAlign={"center"} size='3xl' my={15}>Bienvenido</Heading>
+                <HStack w={"full"} py={'20'}>
+                    {indexButton('Asamblea','/asamblea/ver', HiUserGroup,)}
+                    {indexButton('Historial de Actas', '/archivos/verArchivos', AiFillFile)}
                 </HStack>
-                <HStack w={"full"} py={'28'}>
-                    <Button w={"full"} colorScheme={"teal"} float={"right"} onClick={()=>router.push('/usuarios/ver')} >Ver Usuarios</Button>
-                    <Button w={"full"} colorScheme={"teal"} float={"right"} onClick={()=>router.push('/loguearse/login')} >Iniciar Sesión</Button>
-                    <Button w={"full"} colorScheme={"teal"} float={"right"} onClick={()=>router.push('/loguearse/logout')} >cerrar Sesión</Button>
+                <HStack w={"full"} py={'10'}>
+                    {indexButton('Mandar Avisos', '/enviar_email/email', GrMail)}
+                    {indexButton('Ver Usuarios','/usuarios/ver', FaUserAlt)}
                 </HStack>
             </Container>
         </Box>
